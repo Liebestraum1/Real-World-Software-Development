a729e891a4c9c5f60c3aced843db3b1ba88789ca
psnew14 <psnew14@gmail.com>
2023-12-14 19:57:19 +0900

    Studying Before Making Exporter (61p). 3장 다시 리뷰할 것 (함수형 인터페이스, 계방 / 폐쇄 원칙)

--- chaptor02_bank_transaction/src/main/java/BankStatementProcessor.java
+++ chaptor02_bank_transaction/src/main/java/BankStatementProcessor.java
@@ -1,4 +1,6 @@
+import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementProcessor {
@@ -6,26 +8,40 @@ public class BankStatementProcessor {
     public BankStatementProcessor(List<BankTransaction> bankTransactions){
         this.bankTransactions = bankTransactions;
     }
-    public double calculateTotalAmount(){
-        return bankTransactions
-                .stream()
-                .mapToDouble(BankTransaction :: getAmount)
-                .sum();
+
+    public double summarizeTransactions(BankTransactionSummarizer bankTransactionSummarizer){
+        double result = 0;
+        for(BankTransaction bankTransaction : bankTransactions){
+            result = bankTransactionSummarizer.summarize(result, bankTransaction);
+        }
+        return result;
+    }
+    public double calculateTotalInMonth(Month month){
+        return summarizeTransactions((acc, bankTransaction)
+                -> bankTransaction.getDate().getMonth() == month ? acc + bankTransaction.getAmount() : acc);
     }
 
-    public double calculateTotalInMonth(Month month){
-        return bankTransactions
-                .stream()
-                .filter(b -> b.getDate().getMonth() == month)
-                .mapToDouble(BankTransaction :: getAmount)
-                .sum();
+    public double calculateTotalAmount(){
+        return summarizeTransactions((acc, bankTransaction)
+                -> bankTransaction.getAmount() + acc);
     }
 
     public double calculateTotalForCategory (String category){
-        return bankTransactions
-                .stream()
-                .filter(b -> b.getDescription().equals(category))
-                .mapToDouble(BankTransaction :: getAmount)
-                .sum();
+        return summarizeTransactions((acc, bankTransaction)
+                -> bankTransaction.getDescription().equals(category) ? acc + bankTransaction.getAmount() : acc);
+    }
+
+    public List<BankTransaction> findTransactions(BankTransactionFilter bankTransactionFilter){
+        List<BankTransaction> result = new ArrayList<>();
+        for(BankTransaction bankTransaction : bankTransactions){
+            if(bankTransactionFilter.test(bankTransaction)){
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
+    public List<BankTransaction> findTransactionGreaterThanEqual(int amount) {
+        return findTransactions(bankTransaction -> bankTransaction.getAmount() >= amount);
     }
 }
--- chaptor02_bank_transaction/src/test/java/BankStatementCSVParserTest.java
+++ chaptor02_bank_transaction/src/test/java/BankStatementCSVParserTest.java
@@ -1,6 +1,8 @@
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +11,7 @@ public class BankStatementCSVParserTest {
     private final BankStatementParser statementParser = new BankStatementCSVParser();
 
     @Test
-    public void shouldParseOneCorrectLine() throws Exception {
+    public void shouldParseOneCorrectLine() {
         final String line = "30-12-2023,-10,배달";
         final BankTransaction result = statementParser.parseFrom(line);
         final BankTransaction expected = new BankTransaction(LocalDate.of(2023, Month.DECEMBER, 30), -10, "배달");
@@ -18,5 +20,28 @@ public class BankStatementCSVParserTest {
         Assertions.assertEquals(expected.getDate(), result.getDate());
         Assertions.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
         Assertions.assertEquals(expected.getDescription(), result.getDescription());
+    }
+
+    @Test
+    public void shouldParseMultipleLines() {
+        final List<String> lines = new ArrayList<>();
+        final double tolerance = 0.0d;
+
+        final String line1 = "30-12-2023,-10,배달";
+        final String line2 = "31-12-2023,-20,책";
+        lines.add(line1);
+        lines.add(line2);
+
+        final List<BankTransaction> result = statementParser.parseLinesFrom(lines);
+
+        final List<BankTransaction> expected = new ArrayList<>();
+        expected.add(new BankTransaction(LocalDate.of(2023, Month.DECEMBER, 30), -10, "배달"));
+        expected.add(new BankTransaction(LocalDate.of(2023, Month.DECEMBER, 31), -20, "책"));
+
+        for(int i = 0; i < result.size(); i++){
+            Assertions.assertEquals(expected.get(i).getDate(), result.get(i).getDate());
+            Assertions.assertEquals(expected.get(i).getAmount(), result.get(i).getAmount(), tolerance);
+            Assertions.assertEquals(expected.get(i).getDescription(), result.get(i).getDescription());
+        }
     }
 }
